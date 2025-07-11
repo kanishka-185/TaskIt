cce4d55fd91e318d95191c20f69522aad1d5a3fc
Kanishka-185 <kanishkagrover1@gmail.com>
2025-07-12 04:27:47 +0530

    added sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react'
 
 
-import { Outlet, Route, Routes, useNavigate } from 'react-router-dom'
+import { Navigate, Outlet, Route, Routes, useNavigate } from 'react-router-dom'
 import Layout from './components/Layout';
 import Login from './components/Login'
 import SignUp from './components/SignUp'
+import Dashboard from './pages/Dashboard';
 
 const App = () => {
 
@@ -38,11 +39,11 @@ const App = () => {
     navigate('/login',{ replace: true})
   }
 
-  const ProtectedLayout = () => {
+  const ProtectedLayout = () => (
   <Layout user={currentUser} onLogout={handleLogout}>
     <Outlet />
   </Layout>
-  }
+  )
   return (
     <Routes>
       <Route path='/login' element={<div className='fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center'>
@@ -51,7 +52,11 @@ const App = () => {
       <Route path='/signup' element={<div className='fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center'>
         <SignUp onSubmit={handleAuthSubmit} onSwitchMode= {() => navigate('/login')}/>
       </div>}/>
-      <Route path='/' element={<Layout/> }/>
+      <Route element={currentUser ? <ProtectedLayout/> :
+        <Navigate to='/login' replace/>}>
+        <Route path='/' element={<Dashboard/> }/>
+      </Route>
+      <Route path='*' element={<Navigate to={currentUser ? '/' : '/login'}replace/>}/>
     </Routes>
   )
 }
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -75,13 +75,13 @@ const Login = ({onSubmit,onSwitchMode}) => {
         {
             name: "email",
             type: "email",
-            placeholder: " Email",
+            placeholder: "Email",
             icon: Mail,
         },
         {
             name: "password",
             type: showPassword?"text":"password",
-            placeholder: " Password",
+            placeholder: "Password",
             icon: Lock,
             isPassword: true,
         },
@@ -101,7 +101,7 @@ const Login = ({onSubmit,onSwitchMode}) => {
             <form onSubmit = {handleSubmit} className="space-y-4">
                 {fields.map(({name,type,placeholder,icon:Icon, isPassword}) => (
                     <div key={name} className={INPUTWRAPPER}>
-                        <Icon className='text-purple-500 w-5 h-5'/>
+                        <Icon className='text-purple-500 w-5 h-5 mr-2'/>
                         <input type={type} placeholder={placeholder} value={formData[name]} onChange={(e) => setFormData({...formData , [name]:e.target.value})}
                         className="w-full focus:outline-none text-sm text-gray-700"required/>
 
